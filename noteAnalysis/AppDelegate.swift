c0bb4f86c6c76cc4e4cbcacc8aa791df3120837f
Yuki Wang <yukiluna1999@gmail.com>
2020-02-02 23:03:38 -0800

    Initial Commit

---
+++ noteAnalysis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  noteAnalysis
+//
+//  Created by Yuki Wang on 2/2/20.
+//  Copyright © 2020 Yuki Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ noteAnalysis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  noteAnalysis
+//
+//  Created by Yuki Wang on 2/2/20.
+//  Copyright © 2020 Yuki Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
